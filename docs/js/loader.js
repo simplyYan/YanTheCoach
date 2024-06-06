9235ed05fd24b87d90eb3680d4a0108603bf3914
Wesley Yan Soares Brehmer <102262759+simplyYan@users.noreply.github.com>
2024-06-06 16:22:11 -0300

    Add files via upload

---
+++ docs/js/loader.js
@@ -0,0 +1,113 @@
+
+setTimeout(() => {
+    const anim = new ChainsawAnim('#loader');
+anim.translate(0, 0).rotate(45).scale(1.0).duration('0.5s').end(() => {
+    console.log('Animation completed');
+});
+}, 500);
+setTimeout(() => {
+    const anim = new ChainsawAnim('#loader');
+anim.translate(0, 0).rotate(90).scale(1.0).duration('0.5s').end(() => {
+    console.log('Animation completed');
+});
+}, 1000);
+setTimeout(() => {
+    const anim = new ChainsawAnim('#loader');
+anim.translate(0, 0).rotate(135).scale(1.0).duration('0.5s').end(() => {
+    console.log('Animation completed');
+});
+}, 1500);
+setTimeout(() => {
+    const anim = new ChainsawAnim('#loader');
+anim.translate(0, 0).rotate(180).scale(1.0).duration('0.5s').end(() => {
+    console.log('Animation completed');
+});
+}, 2000);
+setTimeout(() => {
+    const anim = new ChainsawAnim('#loader');
+anim.translate(0, 0).rotate(225).scale(1.0).duration('0.5s').end(() => {
+    console.log('Animation completed');
+});
+}, 2500);
+setTimeout(() => {
+    const anim = new ChainsawAnim('#loader');
+anim.translate(0, 0).rotate(270).scale(1.0).duration('0.5s').end(() => {
+    console.log('Animation completed');
+});
+}, 3000);
+setTimeout(() => {
+    const anim = new ChainsawAnim('#loader');
+anim.translate(0, 0).rotate(315).scale(1.0).duration('0.5s').end(() => {
+    console.log('Animation completed');
+});
+}, 3500);
+setTimeout(() => {
+    const anim = new ChainsawAnim('#loader');
+anim.translate(0, 0).rotate(360).scale(1.0).duration('0.5s').end(() => {
+    console.log('Animation completed');
+});
+}, 4000);setTimeout(() => {
+    const anim = new ChainsawAnim('#loader');
+anim.translate(0, 0).rotate(365).scale(1.0).duration('0.5s').end(() => {
+    console.log('Animation completed');
+});
+}, 4500);
+setInterval(() => {
+    setTimeout(() => {
+        const anim = new ChainsawAnim('#loader');
+    anim.translate(0, 0).rotate(45).scale(1.0).duration('0.5s').end(() => {
+        console.log('Animation completed');
+    });
+    }, 500);
+    setTimeout(() => {
+        const anim = new ChainsawAnim('#loader');
+    anim.translate(0, 0).rotate(90).scale(1.0).duration('0.5s').end(() => {
+        console.log('Animation completed');
+    });
+    }, 1000);
+    setTimeout(() => {
+        const anim = new ChainsawAnim('#loader');
+    anim.translate(0, 0).rotate(135).scale(1.0).duration('0.5s').end(() => {
+        console.log('Animation completed');
+    });
+    }, 1500);
+    setTimeout(() => {
+        const anim = new ChainsawAnim('#loader');
+    anim.translate(0, 0).rotate(180).scale(1.0).duration('0.5s').end(() => {
+        console.log('Animation completed');
+    });
+    }, 2000);
+    setTimeout(() => {
+        const anim = new ChainsawAnim('#loader');
+    anim.translate(0, 0).rotate(225).scale(1.0).duration('0.5s').end(() => {
+        console.log('Animation completed');
+    });
+    }, 2500);
+    setTimeout(() => {
+        const anim = new ChainsawAnim('#loader');
+    anim.translate(0, 0).rotate(270).scale(1.0).duration('0.5s').end(() => {
+        console.log('Animation completed');
+    });
+    }, 3000);
+    setTimeout(() => {
+        const anim = new ChainsawAnim('#loader');
+    anim.translate(0, 0).rotate(315).scale(1.0).duration('0.5s').end(() => {
+        console.log('Animation completed');
+    });
+    }, 3500);
+    setTimeout(() => {
+        const anim = new ChainsawAnim('#loader');
+    anim.translate(0, 0).rotate(360).scale(1.0).duration('0.5s').end(() => {
+        console.log('Animation completed');
+    });
+    }, 4000);setTimeout(() => {
+        const anim = new ChainsawAnim('#loader');
+    anim.translate(0, 0).rotate(365).scale(1.0).duration('0.5s').end(() => {
+        console.log('Animation completed');
+    });
+    }, 4500);
+}, 4500);
+
+setTimeout(() => {
+    window.location.href = "app.html"
+}, 5000);
